1316efe76a2bb50bfe8b28c45153bdc9253a9236
Luis-F-Cerda-P <luis.f.cerda.p@gmail.com>
2026-03-31 02:45:48 -0300

    chore(lint): whitespace

--- app/models/user_status.rb
+++ app/models/user_status.rb
@@ -1,6 +1,6 @@
 class UserStatus < ApplicationRecord
   self.table_name = "UserStatus"
 
-  validates :experience, inclusion: [ "Rookie" , "Veteran" ]
+  validates :experience, inclusion: [ "Rookie", "Veteran" ]
   validates :pending_task_count, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
