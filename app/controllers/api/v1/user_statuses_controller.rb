654aa58af34ded08892a416a6b97fbd96bf3eeb0
Luis-F-Cerda-P <luis.f.cerda.p@gmail.com>
2026-03-31 02:43:04 -0300

    feat(controller): refactor show action into show_or_create
    Leverage the service objects in order to create records when they don't previously exist. The controller could be made thinner in the future, the api_client initialization seems out of place.

--- app/controllers/api/v1/user_statuses_controller.rb
+++ app/controllers/api/v1/user_statuses_controller.rb
@@ -1,14 +1,22 @@
 class Api::V1::UserStatusesController < ApplicationController
-  before_action :set_user_status
+  def show_or_create
+    @user_status = UserStatus.find_by(id: params[:id]) || fetch_and_create
 
-  def show
-    @user_status
+    if @user_status.errors.empty?
+      render :show, status: :ok
+    else
+      render json: @user_status.errors, status: :unprocessable_entity
+    end
   end
 
   private
+    def fetch_and_create
+      client = DummyJson::V1::Client.new
+      fetch_result = UserStatuses::DataFetchingService.call(api_client: client, user_id: params[:id])
+      return fetch_result if fetch_result.failure?
 
-    def set_user_status
-      @user_status = UserStatus.find(params[:id])
+      creation_result = UserStatuses::CreationService.call(fetch_result)
+      creation_result.data if creation_result.success?
     end
 
     def user_status_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get "user_status/:id", to: "user_statuses#show", as: "user_status", format: "json"
+      get "user_status/:id", to: "user_statuses#show_or_create", as: "user_status", format: "json"
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
