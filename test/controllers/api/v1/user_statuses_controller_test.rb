f1a4453274870844000bedc1b85060f4e1b44b00
Luis-F-Cerda-P <luis.f.cerda.p@gmail.com>
2026-03-31 03:15:50 -0300

    test(controller): happy path tests for the show_or_create action
    There is a test Im skipping because I wasnt able to correctly stub an error

--- test/controllers/api/v1/user_statuses_controller_test.rb
+++ test/controllers/api/v1/user_statuses_controller_test.rb
@@ -5,8 +5,26 @@ class Api::V1::UserStatusesControllerTest < ActionDispatch::IntegrationTest
     @user_status = user_statuses(:john_doe)
   end
 
-  test "should get show" do
-    get api_v1_user_status_url(@user_status)
-    assert_response :success
+  test "should show existing user_status without creating a new one" do
+    assert_no_difference("UserStatus.count") do
+      get api_v1_user_status_url(@user_status)
+    end
+
+    assert_response :ok
+  end
+
+  test "should create a user_status when a new id is requested" do
+    user_status = UserStatus.find_by(id: 1)
+    user_status.destroy if user_status
+
+    assert_difference("UserStatus.count") do
+      get api_v1_user_status_url(id: 1)
+    end
+
+    assert_response :ok
+  end
+
+  test "should respond 422 with an error message on validation error" do
+    skip
   end
 end
